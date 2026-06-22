fbedc697fc191eea930d5cc07502903ffbcbeedc
Vraj Mayavat <vrajmayavat08@gmail.com>
2026-06-22 10:17:40 -0400

    added user registration by using JWT authentication

---
+++ job-tracker-server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Job Tracker API is running');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
